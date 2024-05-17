44e5b882687207db3c708062283432437c604b1b
ingediego94 <inge.diego94@gmail.com>
2024-05-16 21:54:27 -0500

    Se agrega referencia a System.Conection

---
+++ BD/Backend/CursoMVC/CapaDatos/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;     // Permite comunicarnos con nuestro archivo Web.config
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        public static string cn = ConfigurationManager.ConnectionStrings["cadena"].ToString();
+
+    }
+}
